d24a984cf991a6544a2be89b0367a80337ff2c21
vhivestate <garcia.valeria001@gmail.com>
2022-09-26 21:52:59 -0600

    nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,21 +4,26 @@ import '../App.css'
 
 function Nav() {
     return (
-        <div>
-        <div>
-            <div>
-            <ul tabIndex={0}>
-            <li><Link to={'/home'}>Home</Link></li>
-            <li><Link to={'/about'}>About</Link></li>
-            <li><Link to={'/gallery'}>Gallery</Link></li>
-            <li><Link to={'/contact'}>Contact</Link></li>
-            </ul>
-            </div>
-        </div>
-        <div className="navbar-end">
-
-        </div>
-        </div>
+<div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost btn-circle">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><Link to={'/home'}>Home</Link></li>
+        <li><Link to={'/about'}>About</Link></li>
+        <li><Link to={'/gallery'}>Gallery</Link></li>
+        <li><Link to={'/contact'}>Contact</Link></li> 
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center">
+    <a className="btn btn-ghost normal-case text-xl">Seaglass</a>
+  </div>
+  <div className="navbar-end">
+  </div>
+</div>
       )
     }
 
