dedc663b1ccfea2fa27909034c450ef28a467ee5
vhivestate <garcia.valeria001@gmail.com>
2022-09-26 22:34:03 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Instagram } from 'react-feather';
+import { GitHub } from 'react-feather';
+import { Twitter } from 'react-feather';
+
 
 function Footer() {
-  return (
-    <div>Footer</div>
-  )
-}
+    
+    return (
+<footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+  <div className="grid grid-flow-col gap-4">
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Gallery</a> 
+  </div> 
+  <div>
+    <div className="grid grid-flow-col gap-4">
+          <a href='https://twitter.com/vhivestate'> <Twitter></Twitter> </a>
 
-export default Footer
+          <a href='https://www.instagram.com/vhive.jpg/'> <Instagram></Instagram> </a>
+
+
+          <a href='https://github.com/vhivestate'> <GitHub></GitHub> </a>
+    </div>
+  </div> 
+  <div>
+    <p>Copyright © 2022 - All right reserved by Vhivestate</p>
+  </div>
+</footer>
+
+    )
+  }
+
+
+export default Footer      
+
