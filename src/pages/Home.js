ea71bfbc387f339d187d039fd967951c8916e9b4
vhivestate <garcia.valeria001@gmail.com>
2022-10-02 21:14:27 -0600

    hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,17 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-    
-    <button className="btn btn-active"><Link to={'/contact'}>link</Link></button>
-    
+    <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md space-x-4">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <button className="btn btn-primary">Serives</button>
+          <button className="btn btn-primary">Book</button>
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
